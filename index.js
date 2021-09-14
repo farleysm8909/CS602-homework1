0c00489910c44638db5646cd7e22925ea0b5f4fd
Sam Farley <samanthafarley32@gmail.com>
2021-09-14 12:31:41 -0400

    error handling, styles

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ document.getElementById("btn").addEventListener("click", function() {
     let chosen = document.getElementById("conversion").value;
     let num = Number(document.getElementById("num").value);
     let result;
-    if (chosen == "fah-to-cel") {
-        result = toCelsius(num);
-    } else if (chosen == "fah-to-kel") {
-        result = fToKelvin(num);
-    } else if (chosen == "cel-to-kel") {
-        result = cToKelvin(num);
-    } else if (chosen == "cel-to-fah") {
-        result = toFahrenheit(num);
+    let type;
+    if (num == "") { // error checking
+        document.getElementById("num").style.background = "#FFCCCC";
+        document.getElementById("results").innerHTML = `<p>Please enter a number</p>`;
+    } else {
+        document.getElementById("num").style.background = "#FFFFFF00"; // resets number field back to white (transparent)
+        if (chosen == "fah-to-cel") {
+            result = toCelsius(num);
+            type = "C";
+        } else if (chosen == "fah-to-kel") {
+            result = fToKelvin(num);
+            type = "K";
+        } else if (chosen == "cel-to-kel") {
+            result = cToKelvin(num);
+            type = "K";
+        } else if (chosen == "cel-to-fah") {
+            result = toFahrenheit(num);
+            type = "F";
+        }
+        let rounded = result.toFixed(2); 
+        document.getElementById("results").innerHTML = `<p>Result: ${rounded}&#176 ${type}</p>`;
     }
-    document.getElementById("results").innerHTML = `<p>Result: ${result}</p>`;
 });
